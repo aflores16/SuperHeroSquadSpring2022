7f0452f823d392d9f25a87d5d3f1eea15b17992f
Andy <aflores16@ggc.edu>
2022-04-08 16:31:37 -0400

    So I created a new method in ReadTextFile so allow to read the Monster Text file, may need work since the Monster Class is yet to be filled

--- src/TextGame/ReadTextFile.java
+++ src/TextGame/ReadTextFile.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class ReadTextFile {
-    public static HashMap<String, Room> createRooms() {
+    public static HashMap<String, Room> createRooms(HashMap <String, Monster> monsters) {
         try {
             // creating a reader to read the text file
             BufferedReader reader = new BufferedReader(new FileReader("Room.txt"));
@@ -42,7 +42,7 @@ public class ReadTextFile {
                 }
 
                 // put new room object and rooms name in HashMap
-                rooms.put(name, new Room(name, roomNum,deckNum,roomId,description, neighbors));
+                rooms.put(name, new Room(name, roomNum,deckNum,roomId,description, neighbors, monsters));
 
                 line = reader.readLine(); // move line to beginning of next room
 
@@ -52,6 +52,62 @@ public class ReadTextFile {
         } catch (IOException e) {
             System.out.println("Room File could not be accessed, try again!");
         }
+        return null;
+    }
+
+    public static HashMap<String, Monster> createMonster() {
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("Monster.txt"));
+            String line = reader.readLine();
+            HashMap<String, Monster> monsters = new HashMap<String, Monster> ();
+
+            while (line != null){
+                String itemID = line ;
+                line = reader.readLine();
+
+                String location = line;
+                line = reader.readLine();
+
+                String name = line;
+                line = reader.readLine();
+
+                String hp = line;
+                Integer health =Integer.valueOf(hp);
+                line = reader.readLine();
+
+                String attack = line;
+                Integer attack_ =Integer.valueOf(attack);
+                line = reader.readLine();
+
+                String foodChance = line;
+                Integer chance = Integer.valueOf(foodChance);
+                line = reader.readLine();
+
+                String item = line;
+                line = reader.readLine();
+
+                String keyChance = line;
+                Integer chanceOfKey = Integer.valueOf(keyChance);
+                line = reader.readLine();
+
+                String keyName = line;
+                line = reader.readLine();
+
+                String description = "";
+
+                while (!line.equals("END")){
+                    description = description + line + '\n';
+                    line = reader.readLine();
+                }
+                monsters.put(name, new Monster(name, health , attack_,location,description, itemID, chance, item, chanceOfKey, keyName ));
+
+                line = reader.readLine();
+            }
+            return monsters;
+        } catch (IOException e) {
+            System.out.println("File not found for monsters");
+        }
+
         return null;
     }
 }
