c30aec2c67443d28dc4d760441e3e9b526f0d338
Andy <aflores16@ggc.edu>
2022-04-11 18:42:24 -0400

    Added inventory and set health to 100 in the player class

--- src/TextGame/Player.java
+++ src/TextGame/Player.java
@@ -1,14 +1,74 @@
 package TextGame;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Player {
     private static String location;
+    private double Health = 100;
+   // private double attackDamage = 0;
+    private static Map<String, Item> inventory = new HashMap<String, Item>();
 
     // setting a location to start
     public Player() {
         location = "Main Hallway 1"; // set to first room
     }
+
+    public void getInventory() {
+        if (inventory.isEmpty()) { //if inventory is empty
+            System.out.println("Inventory is empty, try to find items");
+        } else { //Looks for items based the current inventory
+            for (Map.Entry<String, Item> elt : inventory.entrySet()) {
+                System.out.println(elt.getKey());
+            }
+        }
+    }
+
+    public void add(String item, HashMap<String, Room> rooms) {
+        // check whether object is in room you are in
+        Room current = rooms.get(location);
+
+        Item temp = null;
+
+        if (current.getInventory().containsKey(item)) { // item is in room
+            temp = current.getInventory().get(item);
+
+            current.getInventory().remove(item);
+            inventory.put(temp.getItemName(), temp);
+            System.out.println(item + " was successfully added to your inventory !");
+        } else { //else
+            System.out.println( item + " is not in the room.");
+        }
+    }
+
+    public void drop(String item, HashMap<String, Room> rooms) {
+        Item temp = null;
+
+        if (inventory.containsKey(item)) { //if item is in inventory
+            temp = inventory.get(item);
+            inventory.remove(item);
+
+            Room current = rooms.get(location); // where the player is currently at
+
+            current.getInventory().put(temp.getItemName(), temp);
+            System.out.println(item + " was successfully dropped");
+        } else { // else
+            System.out.println("Sorry, " + item + " is not in your inventory.");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // created a movement function to allow player to travel through rooms based on exits
     public void movement(String direction, HashMap<String, Room> rooms) {
@@ -50,6 +110,8 @@ public class Player {
         }
 
     }
+
+
 
     public String getLocation() {
         return location;
